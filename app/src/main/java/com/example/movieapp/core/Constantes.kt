3fe83da9befc1c47f2f9baee990d88763b5a4738
ADRIELCELSOROSALES <programacionIPET424@gmail.com>
2024-06-04 18:04:37 -0300

    Initial commit

---
+++ app/src/main/java/com/example/movieapp/core/Constantes.kt
@@ -0,0 +1,13 @@
+package com.example.movieapp.core
+
+object Constantes {
+    const val API_KEY = "6e0ad3a58f0a04087bded3cb34c4543f"
+    const val BASE_URL = "https://api.themoviedb.org/3/movie/"
+    const val BASE_URL_IMAGE = "https://image.tmdb.org/t/p/w500/"
+
+    const val IMAGEN_ANCHO = 330
+    const val IMAGEN_ALTO = 330
+
+    const val MAX_CALIFICACION = 10.0
+
+}
